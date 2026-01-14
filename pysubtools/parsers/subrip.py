ee02427da128284025750a15b8aa92b282f01443
Grimpy101 <gorupgorazd@gmail.com>
2026-01-15 00:06:20 +0100

    Remove unused imports

--- pysubtools/parsers/subrip.py
+++ pysubtools/parsers/subrip.py
@@ -1,10 +1,4 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import re
-import io
 from state_machine import (
     acts_as_state_machine,
     before,
