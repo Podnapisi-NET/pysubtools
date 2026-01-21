77b549916df466b819ecfe7d15766e1f8f640b8f
Gorazd Gorup <gorupgorazd@gmail.com>
2026-01-21 21:05:35 +0100

    Add tags for linters

--- pysubtools/parsers/__init__.py
+++ pysubtools/parsers/__init__.py
@@ -2,8 +2,8 @@ from .base import Parser, NoParserError, ParseError
 from . import encodings
 
 # To load all parser
-from . import subrip
-from . import microdvd
+from . import subrip  # noqa: F401
+from . import microdvd  # noqa: F401
 
 __all__ = [
     "Parser",
