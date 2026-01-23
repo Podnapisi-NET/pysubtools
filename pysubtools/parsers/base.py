031c463b13cb8b42b0cfe7b033a495d68784431d
Gorazd Gorup <gorupgorazd@gmail.com>
2026-01-23 23:12:38 +0100

    Annotate parser base with types

--- pysubtools/parsers/base.py
+++ pysubtools/parsers/base.py
@@ -1,4 +1,5 @@
 import io
+import typing
 
 from . import encodings
 
@@ -8,52 +9,59 @@ class NoParserError(Exception):
 
 
 class ParseError(Exception):
-    def __init__(self, line_number, column, line, description):
+    def __init__(self, line_number: int, column: int, line: str, description: str):
         self.line_number = line_number
         self.column = column
         self.line = line
         self.description = description
         super(ParseError, self).__init__(self.description)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self)
 
-    def __unicode__(self):
+    def __unicode__(self) -> str:
         return "Parse error on line {} at column {} error occurred '{}'".format(
             self.line_number, self.column, self.description
         )
 
 
 class ParseWarning(ParseError):
-    def __unicode__(self):
+    def __unicode__(self) -> str:
         return "Parse warning on line {} at column {} warning occurred '{}'".format(
             self.line_number, self.column, self.description
         )
+
+
+class ParserErrorMessage(typing.TypedDict):
+    line_number: int
+    col: int
+    line: str
+    description: str
 
 
 class Parser(object):
     """Abstract class for all parsers."""
 
     LEVELS = ("warning", "error")
-    FORMAT = ""
-    _subtitle = None
-    _stop_level = "error"
+    FORMAT: str = ""
+    _subtitle: typing.Optional[typing.Any] = None
+    _stop_level: str = "error"
     parsed = None
-    encoding = None
+    encoding: typing.Optional[str] = None
     encoding_confidence = None
 
-    def __init__(self, stop_level="error"):
-        self.warnings = []
-        self.errors = []
+    def __init__(self, stop_level: str = "error"):
+        self.warnings: typing.List[ParserErrorMessage] = []
+        self.errors: typing.List[ParserErrorMessage] = []
         self._data = None
-        self._stop_level = stop_level
+        self._stop_level: str = stop_level
 
         # Part of the parser internals
-        self._read_lines = []
-        self._current_line_num = -1
-        self._current_line = None
+        self._read_lines: typing.List[typing.Union[str, bytes]] = []
+        self._current_line_num: int = -1
+        self._current_line: typing.Optional[typing.Union[str, bytes]] = None
 
-    def _add_msg(self, level, line_number, column, line, description):
+    def _add_msg(self, level: str, line_number: int, column: int, line: str, description: str):
         if self._stop_level and self.LEVELS.index(level) >= self.LEVELS.index(
             self._stop_level
         ):
@@ -65,7 +73,7 @@ class Parser(object):
         line = str(line)
         description = str(description)
 
-        msg = {
+        msg: ParserErrorMessage = {
             "line_number": int(line_number),
             "col": int(column),
             "line": line,
@@ -84,7 +92,7 @@ class Parser(object):
         self._add_msg("error", *args, **kwargs)
 
     @staticmethod
-    def _normalize_data(data):
+    def _normalize_data(data: typing.Union[bytes, io.BytesIO, io.BufferedReader]) -> typing.Union[io.BytesIO, io.BufferedReader]:
         if isinstance(data, bytes):
             data = io.BytesIO(data)
         elif not isinstance(data, (io.BytesIO, io.BufferedReader)):
@@ -93,27 +101,27 @@ class Parser(object):
         return data
 
     @classmethod
-    def can_parse(cls, data):
+    def can_parse(cls, data: typing.Union[bytes, io.BytesIO, io.BufferedReader]) -> bool:
         data = cls._normalize_data(data)
         return cls._can_parse(data)
 
     @classmethod
-    def _can_parse(cls, data):
+    def _can_parse(cls, data: typing.Union[bytes, io.BytesIO, io.BufferedReader]) -> bool:
         """Needs to be reimplemented to quickly check if file seems the proper format."""
         raise NotImplementedError
 
-    def _parse(self):
+    def _parse(self) -> typing.Iterable[typing.Any]:
         """
         Parses the file, it returns a list of units in specified format. Needs to be
         implemented by the parser. It can also be a generator (yield)
         """
         raise NotImplementedError
 
-    def _parse_metadata(self):
+    def _parse_metadata(self) -> typing.Dict[str, typing.Any]:
         """Parses the subtitle metadata (if format has a header at all)."""
         return {}
 
-    def parse(self, data=None, encoding=None, language=None, **kwargs):
+    def parse(self, data: typing.Optional[typing.Union[io.BytesIO, io.BufferedReader]] = None, encoding: typing.Optional[str] = None, language: typing.Optional[str] = None, **kwargs) -> typing.Any:
         """Parses the file and returns the subtitle. Check warnings after the parse."""
         if data:
             # We have new data, discard old and set up for new
@@ -133,9 +141,8 @@ class Parser(object):
                 self._data, self.encoding, newline="", errors="replace"
             )
 
-        # Create subtitle
         from .. import Subtitle, SubtitleUnit
-
+        
         sub = Subtitle(**self._parse_metadata())
         for unit in self._parse(**kwargs):
             try:
@@ -151,7 +158,7 @@ class Parser(object):
         return sub
 
     @staticmethod
-    def from_data(data, encoding=None, language=None, **kwargs):
+    def from_data(data: typing.Union[bytes, io.BytesIO, io.BufferedReader], encoding: typing.Optional[str] = None, language: typing.Optional[str] = None, **kwargs) -> 'Parser':
         """Returns a parser that can parse 'data' in raw string."""
         data = Parser._normalize_data(data)
         encoding, encoding_confidence = encodings.detect(data, encoding, language)
@@ -170,7 +177,7 @@ class Parser(object):
         raise NoParserError("Could not find parser.")
 
     @staticmethod
-    def from_format(format, **kwargs):
+    def from_format(format: str, **kwargs) -> 'Parser':
         """Returns a parser with 'name'."""
         for parser in Parser.__subclasses__():
             if parser.FORMAT == format:
@@ -187,7 +194,7 @@ class Parser(object):
                 pass
 
     # Iteration methods
-    def _next_line(self):
+    def _next_line(self) -> bool:
         line = self._data.readline() if self._data else None
         if not line:
             return False
@@ -198,13 +205,13 @@ class Parser(object):
 
         return True
 
-    def _fetch_line(self, line):
+    def _fetch_line(self, line: int) -> typing.Union[str, bytes]:
         if line > self._current_line_num:
             raise ValueError("Cannot seek forward.")
 
         return self._read_lines[line].rstrip()
 
-    def _rewind(self):
+    def _rewind(self) -> None:
         self._current_line_num = -1
         self._read_lines = []
         self._current_line = None
