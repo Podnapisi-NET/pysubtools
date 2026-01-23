7cd34ac80a161920d1651f38a46c6c0ecc27219e
Gorazd Gorup <gorupgorazd@gmail.com>
2026-01-23 22:19:14 +0100

    Remove Python 2 code, adjust read1 function
    read1 function was updated to match GzipFile function definition.

--- pysubtools/utils.py
+++ pysubtools/utils.py
@@ -1,5 +1,4 @@
 import gzip
-import sys
 
 
 class PatchedGzipFile(gzip.GzipFile):
@@ -8,20 +7,13 @@ class PatchedGzipFile(gzip.GzipFile):
     around it. Not needed in Python 3.3+
     """
 
-    def read1(self, n):
-        return self.read(n)
+    def read1(self, size: int = -1) -> bytes:
+        return self.read(size)
 
 
 class UnicodeMixin(object):
     """Mixin class to handle defining the proper __str__/__unicode__
     methods in Python 2 or 3."""
 
-    if sys.version_info[0] >= 3:  # Python 3
-
-        def __str__(self):
-            return self.__unicode__()
-
-    else:  # Python 2
-
-        def __str__(self):
-            return self.__unicode__().encode("utf8")
+    def __str__(self):
+        return self.__unicode__()
