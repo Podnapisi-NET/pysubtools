9a8e4e5f3977b72cd885674a23286be01cb07c82
Gorazd Gorup <gorupgorazd@gmail.com>
2026-01-24 18:53:23 +0100

    Bring back the zipfile import

--- pysubtools/__init__.py
+++ pysubtools/__init__.py
@@ -2,6 +2,8 @@ from . import parsers
 from . import exporters
 from .subtitle import Subtitle, SubtitleUnit, SubtitleLine
 
+from zipfile import ZipExtFile
+
 __all__ = [
     "Subtitle",
     "SubtitleUnit",
